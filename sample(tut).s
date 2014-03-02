0996214d6e2252fcaa8be3f304953960d4e77b24
BboyMUPO <sarkanyalex214@gmail.com>
2014-03-02 16:47:20 +0100

    Create sample(tut).s

---
+++ sample(tut).s
@@ -0,0 +1,4 @@
+MOVS R1,#6 ;adds a value 6 the second general prpose register(R0-R12 general propose registers)
+ADDS R1,#1 ;add +1 to R1 so that =7
+CMP R1,#7 ;checkss if R1 = 7?
+SUBS R1,#2 ;subtract a 2 from R1
