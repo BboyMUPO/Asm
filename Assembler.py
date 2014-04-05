4dbb4c57f3bf8851ce4b096c0500106f7d87b640
BboyMUPO <sarkanyalex214@gmail.com>
2014-04-05 20:45:12 +0200

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -1,6 +1,6 @@
-#A program that converts assembly code to machine code
+#A program that converts armv6-m assembly code to machine code
 #Works line by line
-#For now only with a low support
+#For now only with a low function support
 #Don't converts decimal numbers to binary..converter avilible here:https://github.com/BboyMUPO/Convs/blob/master/decimal2binary
 
 import re
@@ -16,6 +16,32 @@ def process_line(line):
     line = re.sub('PC', '0b1111', line)
               
     line = re.sub('SP', '0b1101', line)
+    
+    line = re.sub('R0', '0b00000', line)
+    
+    line = re.sub('R1', '0b00001', line)
+    
+    line = re.sub('R2', '0b00010', line)
+    
+    line = re.sub('R3', '0b00011', line)
+    
+    line = re.sub('R4', '0b00100', line)
+    
+    line = re.sub('R5', '0b00101', line)
+    
+    line = re.sub('R6', '0b00110', line)
+    
+    line = re.sub('R7', '0b00111', line)
+    
+    line = re.sub('R8', '0b01000', line)
+    
+    line = re.sub('R9', '0b01001', line)
+    
+    line = re.sub('R10', '0b01010', line)
+    
+    line = re.sub('R11', '0b01011', line)
+    
+    line = re.sub('R12', '0b01100', line)
     
     line = re.sub('BLX', '010001111', line)
     
